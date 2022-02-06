3d69c1735a872484a320ac7bc813c755fc50b827
Nikolay Ovcharov <n_ovcharov@yahoo.com>
2022-02-06 16:22:57 +0200

    fix image upload

--- Aurelia/Aurelia.App/Controllers/ProductController.cs
+++ Aurelia/Aurelia.App/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using Aurelia.App.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Hosting;
 using System.Collections.Generic;
 
 namespace Aurelia.App.Controllers
@@ -11,10 +12,12 @@ namespace Aurelia.App.Controllers
     public class ProductController: Controller
     {
         private  ApplicationDbContext _aureliaDb;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(ApplicationDbContext db)
+        public ProductController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _aureliaDb = db;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult CreateProduct()
         {
@@ -25,31 +28,43 @@ namespace Aurelia.App.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> CreateProduct(Product product, IFormFile file)
+        public async Task<ActionResult> CreateProduct(ProductViewModel model)
         {
-            
-                //validaciq
-                string fileName = $"{Directory.GetCurrentDirectory()}{@"\Images"}" + "\\" + file.FileName;
-                using (FileStream fileStream = System.IO.File.Create(fileName))
-                {
-                    file.CopyTo(fileStream);
-                    fileStream.Flush();
-                }
-                product.Image = fileName;
-                
+            string uniqueFileName = UploadedFile(model);
+            Product product = new Product
+            {
+                ProductName = model.ProductName,
+                Description = model.Description,
+                Price = model.Price,
+                Quantity = model.Quantity,
+                ProductCategory = model.ProductCategory,
+                ProductCategoryId = model.ProductCategoryId,
+                Brand = model.Brand,
+                Image = uniqueFileName
 
-                if (ModelState.IsValid) 
-                {
-                    _aureliaDb.Products.Add(product);
-                    await _aureliaDb.SaveChangesAsync();
-                    return View("CreateProduct");
-                }
+            };
+            _aureliaDb.Products.Add(product);
+            await _aureliaDb.SaveChangesAsync();
             return View("Index");
-
            
 
         }
-       
+        private string UploadedFile(ProductViewModel model)
+        {
+            string uniqueFileName = null;
+
+            if (model.ProductImage != null)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = model.ProductName + "_" + model.ProductImage.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.ProductImage.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
+        }
 
     }
 }
---
+++ Aurelia/Aurelia.App/Models/ProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aurelia.App.Models
+{
+    public class ProductViewModel
+    {
+
+        [Required(ErrorMessage = "Please enter Product Name")]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Product Quantity")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Please enter Product Price")]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please choose category")]
+        public string? ProductCategoryId { get; set; }
+
+        public virtual ProductCategories? ProductCategory { get; set; }
+
+        [Required(ErrorMessage = "Please enter Product Brand")]
+        public string Brand { get; set; }
+        [Required(ErrorMessage = "Please choose Product Image")]
+        public IFormFile ProductImage { get; set; }
+    }
+}
